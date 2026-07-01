e9d9af2a2026c5e85d5a447c70bfed9b71029288
Viv3k-008 <singhvivg2n@gmail.com>
2026-07-02 00:53:22 +0530

    Changes in sendEmail

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,8 +4,8 @@ const nodeMailer = require("nodemailer");
    try {
      const transporter = nodeMailer.createTransport({
         host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
         family: 4, // force IPv4 to avoid Render's IPv6 ENETUNREACH issue
         auth: {
           user: process.env.EMAIL_USER,
